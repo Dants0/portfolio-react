abc0b983f82b21fc8e25d45341a04e323a53a9c4
Dants0 <gui.dg1@hotmail.com>
2022-02-26 14:07:01 -0300

    every changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import { useContext } from "react";
-import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
-import Intro from "./components/intro/Intro";
-import ProductList from "./components/productList/ProductList";
-import Toggle from "./components/toggle/Toggle";
+import Intro from './components/intro/Intro'
+import Toggle from './components/toggle/Toggle'
 import { ThemeContext } from "./context";
 
 const App = () => {
-  const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
   return (
     <div
-      style={{
+    style={{
         backgroundColor: darkMode ? "#222" : "white",
         color: darkMode && "white",
       }}
     >
-      <Toggle />
-      <Intro />
-      <About />
-      <ProductList />
-      <Contact />
+        <Toggle/>
+        <Intro/>
     </div>
-  );
-};
-
-export default App;
+  )
+}
+export default App
