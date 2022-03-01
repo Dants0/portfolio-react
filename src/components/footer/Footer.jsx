8d1decce5424bcd4f125b25bdf08568e249d4458
Dants0 <gui.dg1@hotmail.com>
2022-03-01 00:13:26 -0300

    finalizando footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,8 +7,16 @@ const Footer = () => {
         <div className="f-bg">
             <div className="f-wrapper">
                 <div className="f-bottom">
-                    <div className="f-copyright">
-                        <h1>Dants0</h1>
+                    <div className="f-items">
+                        <p>Social Links</p>
+                        <ul>
+                          <li className="items">
+                          <img src="https://img.icons8.com/ios-glyphs/30/000000/github.png"/><a href="https://github.com/Dants0">Github</a>
+                          </li>
+                          <li className="items">
+                          <img src="https://img.icons8.com/material-outlined/24/000000/instagram-new--v1.png"/><a href="https://www.instagram.com/dantas_44/">Instagram</a>
+                          </li>
+                        </ul>
                     </div>
                 </div>
             </div>
