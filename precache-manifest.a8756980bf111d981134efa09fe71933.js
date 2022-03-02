aef1d97861b712f0fd3a332cb68fc0c7958defbe
Dants0 <gui.dg1@hotmail.com>
2022-03-02 20:07:40 -0300

    Updates

---
+++ precache-manifest.a8756980bf111d981134efa09fe71933.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "07a83ba7fb58a5ebba9c",
+    "url": "./static/css/main.bcca1891.chunk.css"
+  },
+  {
+    "revision": "07a83ba7fb58a5ebba9c",
+    "url": "./static/js/main.14509ec1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ee21ba125def526d095f",
+    "url": "./static/js/2.12d49548.chunk.js"
+  },
+  {
+    "revision": "3412a318da704d525e88ce0b685e5d0c",
+    "url": "./static/media/gitandgithubcourse.3412a318.png"
+  },
+  {
+    "revision": "a9975ab5f2ff338d9b43c2b71f3db041",
+    "url": "./static/media/unity3dcertificate.a9975ab5.png"
+  },
+  {
+    "revision": "bb4da7c80be7d23be92c4b8ad25bec46",
+    "url": "./static/media/webmodern.bb4da7c8.png"
+  },
+  {
+    "revision": "e4e1c695dee8a465e8076f5695aaab96",
+    "url": "./static/media/wrench.e4e1c695.png"
+  },
+  {
+    "revision": "fb2ed1c2c2ad1ba183568ec2323a6ffe",
+    "url": "./static/media/email.fb2ed1c2.png"
+  },
+  {
+    "revision": "5d5af2b00d7c74ebe3f5f20af73ce62c",
+    "url": "./static/media/address.5d5af2b0.png"
+  },
+  {
+    "revision": "aea39a157ce83bdad4ba44d20889fbba",
+    "url": "./static/media/linkedin.aea39a15.png"
+  },
+  {
+    "revision": "2550e4a51d9db6e0ae2a30e710ecbe2b",
+    "url": "./static/media/me.2550e4a5.png"
+  },
+  {
+    "revision": "9ee7a0e513da02594e35b04e65bccbcd",
+    "url": "./static/media/light.9ee7a0e5.png"
+  },
+  {
+    "revision": "2c381230279580b48be62622a180db41",
+    "url": "./static/media/moon.2c381230.png"
+  },
+  {
+    "revision": "38dce30004edecb42537d606a5ecc92c",
+    "url": "./index.html"
+  }
+];
